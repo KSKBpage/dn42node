f2f5d2bda678d01e8bcb51bf06cd927f994e7712
日下部 詩 <kusakabesi2018@gmail.com>
2021-12-15 08:54:18 +0800

    test

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+. $LOC.env
+export DOLLAR='$'
+cd public
+for template in *.template; do
+    [ -f "$template" ] || continue
+    conf=${template::-9}
+    cat $template | sed -e 's/\${/＄/g' | sed 's/\$/${DOLLAR}/g' | sed 's/＄/${/g' | envsubst > $conf
+done
