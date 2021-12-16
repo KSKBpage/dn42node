84da21ca6b818a54f6eaabfcff4ec88e1afb482c
日下部 詩 <kusakabesi2018@gmail.com>
2021-12-16 16:13:49 +0800

    Update build.sh
    Signed-off-by: KusakabeSi <si@kskb.eu.org>

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -x
 set -a
-. $LOC.env
+
 export DOLLAR='$'
-cd public
-for template in *.template; do
-    [ -f "$template" ] || continue
-    conf=${template::-9}
-    cat $template | sed -e 's/\${/＄/g' | sed 's/\$/${DOLLAR}/g' | sed 's/＄/${/g' | envsubst > $conf
+
+for theenv in *.env; do
+    . $theenv
+    folder=${theenv::-4}
+    cd public
+    mkdir -p $folder
+    for template in *.template; do
+        [ -f "$template" ] || continue
+        conf=${template::-9}
+        cat $template | sed -e 's/\${/＄/g' | sed 's/\$/${DOLLAR}/g' | sed 's/＄/${/g' | envsubst > $folder/$conf
+    done
+    cd ..
 done
