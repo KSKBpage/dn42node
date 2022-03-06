81c238290c7cc7d089cc1c99eb39638646ec5245
日下部 詩 <kusakabesi2018@gmail.com>
2022-03-07 04:09:59 +0800

    isolate vars

--- build.sh
+++ build.sh
@@ -5,14 +5,15 @@ set -a
 export DOLLAR='$'
 
 for theenv in *.env; do
-    . $theenv
-    folder=${theenv::-4}
-    cd public
-    mkdir -p $folder
-    for template in *.template; do
-        [ -f "$template" ] || continue
-        conf=${template::-9}
-        cat $template | sed -e 's/\${/＄/g' | sed 's/\$/${DOLLAR}/g' | sed 's/＄/${/g' | envsubst > $folder/$conf
-    done
-    cd ..
+eval $( source $theenv;
+        folder=${theenv::-4};
+        cd public;
+        mkdir -p $folder;
+        for template in *.template; do
+            [ -f "$template" ] || continue
+            conf=${template::-9}
+            cat $template | sed -e 's/\${/＄/g' | sed 's/\$/${DOLLAR}/g' | sed 's/＄/${/g' | envsubst > $folder/$conf
+        done
+    )
+
 done
